ec55288bc2c24866fb6d93030c5113287dcdf605
Shivika Kejriwal <shivika.kejriwal2000@gmail.com>
2021-02-26 18:47:51 +0530

    Use of final keyword in java

---
+++ Employee.java
@@ -0,0 +1,23 @@
+public class Employee
+{
+    final int Salary;
+    Employee(int i)
+    
+    {
+    	Salary=i;
+    }
+    
+    void description()
+    {
+       System.out.println("Salary of the employee is "+Salary);
+    }
+    
+	public static void main(String[] args)
+	
+	{
+		Employee ob=new Employee(70000);
+		ob.description();
+
+	}
+
+}
