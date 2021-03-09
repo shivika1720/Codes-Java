8b7196753ef151bf28b1caeb7c6d69d3f434d299
Shivika Kejriwal <59910256+shivika1720@users.noreply.github.com>
2021-03-09 22:32:37 +0530

    Update Hey.java

--- Hey.java
+++ Hey.java
@@ -1,8 +1,10 @@
-package basics;
+package basics; 
 
-public class Hey {
+public class Hey
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		int a = 0b100_00_00_000;
 		  
 		System.out.println(a);
