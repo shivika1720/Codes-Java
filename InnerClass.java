a0a0ae7dee200f5378fc11b352d40b7a69f5cb3d
Shivika Kejriwal <shivika.kejriwal2000@gmail.com>
2021-03-12 22:40:51 +0530

    Inner classes in java

---
+++ InnerClass.java
@@ -0,0 +1,29 @@
+class Outer
+{
+	int i;
+	void show()
+	{
+		System.out.println("Show");
+	}
+	class Inner              
+	{
+		public void display()
+		{
+			System.out.println("Display");
+		}
+	
+	}
+}
+public class InnerClass
+{
+	public static void main(String[] args)
+	{
+		Outer ob= new Outer();
+		ob.show();
+		
+		Outer.Inner ob1= ob.new Inner();  // If inner class will be static, no need to create object.
+		ob1.display();
+		
+		
+	}
+}
