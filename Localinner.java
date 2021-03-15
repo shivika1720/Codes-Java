04ebb430dd0d5dbdcdbf79298761259ef4d134f3
Shivika Kejriwal <shivika.kejriwal2000@gmail.com>
2021-03-15 23:14:55 +0530

    Local Inner class in java

---
+++ Localinner.java
@@ -0,0 +1,24 @@
+public class Localinner 
+ {
+		 void create()
+		 {
+			 System.out.println("This is march goin'");
+			 
+		      class English
+		      {
+		    	  void expand()
+		    	  {
+		    	     System.out.println("This is march goin'");
+		    	  }
+		      }
+		         English en=new English();
+		         en.expand();
+		  }
+ 
+        public void main(String[] args)
+     {
+        	Localinner lo =new Localinner();
+    	    lo.create();
+    	 
+     }
+  }
