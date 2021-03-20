f0f467e16954cdc3b0db8a11b997368b0ee5b525
Shivika Kejriwal <59910256+shivika1720@users.noreply.github.com>
2021-03-20 23:04:24 +0530

    Use of this and super keyword

--- Fruits.java
+++ Fruits.java
@@ -7,7 +7,7 @@ class Fruits
     int no_of_fruits=5;
     void count()
     {
-      System.out.println(no_of_fruits);
+      System.out.println(no_of_fruits);   // output- 10
       System.out.println(this.no_of_fruits);
       System.out.println(super.no_of_fruits);
     }
