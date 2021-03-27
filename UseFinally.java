7437fd836c8ec967585233b378bfc9eecb0c7407
Shivika Kejriwal <59910256+shivika1720@users.noreply.github.com>
2021-03-27 23:11:58 +0530

    Finally block in java

---
+++ UseFinally.java
@@ -0,0 +1,26 @@
+
+public class UseFinally {
+
+	public static void main(String[] args)
+	{
+		try        // Unchecked Exception
+		{
+			int a=100, b=0, c;
+			c=a/b;
+			System.out.println(c);
+		}
+		catch(AssertionError e)
+		{
+			System.out.println(e);
+			System.out.println("Display");
+		}
+		finally
+		{
+			System.out.println("YOU HAVE TO PRINT THIS");
+		}
+		
+
+	}
+
+}
+
