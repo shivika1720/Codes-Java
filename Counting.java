5c898a6ea1f0176fba739222fc41b6ed80d8f241
Shivika Kejriwal <shivika.kejriwal2000@gmail.com>
2021-04-05 22:53:17 +0530

    frequency in a string

---
+++ Counting.java
@@ -0,0 +1,26 @@
+public class Counting
+{
+	public static void main(String[] args)
+	{  
+		String str = "water pollution is the contamination of water bodies usullay by the action of human activities water pollution happens when toxic substances enter in the water";
+		int[] fre = new int[str.length()];  
+		int a, b;  
+  
+		char string[] = str.toCharArray();  
+
+		for(a = 0; a <str.length(); a++)
+		
+		{  
+			fre[a] = 1;  
+			for(b = a+1; b <str.length(); b++)
+			{  
+				if(string[a] == string[b])
+				{  
+					fre[a]++;  
+
+					
+					string[b] = '0';  
+				}  
+			}  
+		}  
+
