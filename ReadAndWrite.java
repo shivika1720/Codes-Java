3cccd39a7149f3a0405b611e0c8d0e3a4ef95d56
Shivika Kejriwal <shivika.kejriwal2000@gmail.com>
2021-04-13 23:34:48 +0530

    Throws keyword

---
+++ ReadAndWrite.java
@@ -0,0 +1,45 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+
+public class ReadAndWrite
+{
+   void readFile() throws FileNotFoundException
+   {
+	FileInputStream fis= new FileInputStream("d:/abc.txt");
+	
+   }
+
+   void writeFile() throws FileNotFoundException
+   {
+	FileOutputStream fos= new FileOutputStream("d:/xyz.txt");
+	
+   }
+}
+
+class Test
+ {
+    public static void main(String[] args)
+    {
+    	ReadAndWrite rw= new ReadAndWrite();
+    	try
+    	{
+    		rw.readFile();
+    	}
+    	catch(FileNotFoundException e)
+    	{
+    		e.printStackTrace();
+    	}
+    	System.out.println("hello");
+    	try
+    	{
+    		rw.writeFile();
+    	}
+    	catch(FileNotFoundException e)
+    	{
+    		e.printStackTrace();
+    	}
+    	System.out.println("hey");
+    }
+ }
+
